b31dcfae1f7ef7d470b46c8f60728ff8574d08f6
cooldragontattoo <troy.rttr@gmail.com>
2025-04-24 10:24:01 -0600

    remove agent

--- examples/hello_henkins.groovy
+++ examples/hello_henkins.groovy
@@ -2,22 +2,22 @@ pipeline {
     options {
         skipDefaultCheckout()
     }
-    agent {
-        kubernetes {
-            defaultContainer: 'testing'
-            yaml '''
-kind: Pod
-spec: 
-  containers:
-  - name: testing
-    image: python:3.8
-    imagePullPolicy: Always
-    command:
-    - cat
-    tty: true
-'''    
-        }
-    }
+//     agent {
+//         kubernetes {
+//             defaultContainer: 'testing'
+//             yaml '''
+// kind: Pod
+// spec: 
+//   containers:
+//   - name: testing
+//     image: python:3.8
+//     imagePullPolicy: Always
+//     command:
+//     - cat
+//     tty: true
+// '''    
+//         }
+//     }
     stages{
         stage('Greetings') {
             steps {

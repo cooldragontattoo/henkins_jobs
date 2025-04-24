da987262654cb38620bcec7e5dbd43fd89397d53
cooldragontattoo <troy.rttr@gmail.com>
2025-04-24 09:14:46 -0600

    first commit

---
+++ examples/hello_henkins.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    options {
+        skipDefaultCheckout()
+    }
+    agent {
+        kubernetes {
+            defaultContainer: 'testing'
+            yaml '''
+kind: Pod
+spec: 
+  containers:
+  - name: testing
+    image: python:3.8
+    imagePullPolicy: Always
+    command:
+    - cat
+    tty: true
+'''    }
+    stages{
+        stage('Greetings') {
+            steps {
+                echo 'Hello Henkins'
+            }
+        }
+    }
+}

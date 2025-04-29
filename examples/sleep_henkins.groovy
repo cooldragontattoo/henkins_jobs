f9d976885d1e4ca65627bcfe82b2db17329cf633
cooldragontattoo <troy.rttr@gmail.com>
2025-04-29 13:23:44 -0600

    add some out while the job is catching zzz's

--- examples/sleep_henkins.groovy
+++ examples/sleep_henkins.groovy
@@ -9,7 +9,12 @@ pipeline {
     stages{
         stage('Sleepy Time') {
             steps {
-                sleep 3600
+                script {
+                    for (int i = 1; i <= 720; i++) {
+                        echo "Sleeping... second ${i * 5} of 3600"
+                        sleep time: 5, unit: 'SECONDS'
+                    }
+                }
             }
         }
     }
